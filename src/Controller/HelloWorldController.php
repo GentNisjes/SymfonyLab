dcd0cfb60a2428c468eaef2911466879356274cc
Stijn <stijngeens07@gmail.com>
2025-03-05 13:02:39 +0100

    Started with the symfony dev

--- src/Controller/HelloWorldController.php
+++ src/Controller/HelloWorldController.php
@@ -1,8 +1,23 @@
 <?php
 
+declare(strict_types=1);
+
+// src/Controller/HelloWorldController.php
 namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class HelloWorldController
 {
+    // Route that accepts a 'name' parameter
 
+    #[Route("/welcome/{name}")]
+
+    public function welcome($name): Response
+    {
+        // Create an HTML response with a personalized welcome message
+        return new Response('<html><body>Hello ' . htmlspecialchars($name) . ', welcome to Symfony!</body></html>');
+    }
 }
+
